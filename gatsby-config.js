1e3440966c3ff631bb9104e9f36804a447442e79
Michał Starski <michalstarski99@gmail.com>
2020-03-08 00:46:38 +0100

    Configured ESLint to work with react

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-sass`,
       options: {
